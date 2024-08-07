a8c59d6cc03fbbf35a486a1d5d3ef62d4ef7b0b8
0x476569636f <darknet1255@gmail.com>
2024-08-08 03:48:34 +0700

    removed unused import statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { Footer } from "@/components/layout/Footer";
 import { Navbar } from "@/components/layout/Navbar";
 
