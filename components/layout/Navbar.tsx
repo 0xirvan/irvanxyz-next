6d8addbc87fcc671893f924f1feb9a5f7211b0f5
0x476569636f <darknet1255@gmail.com>
2024-08-08 07:57:36 +0700

    chore: update Navbar component with responsive behavior and add ThemeToggle

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,23 +1,46 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { siteConfig } from "@/config/siteConfig";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const handleClick = () => {
+    setNavbar(false);
+  };
+
+  useEffect(() => {
+    if (navbar) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [navbar]);
   return (
     <nav className="select-none bg-background px-18">
       <div className="flex justify-between">
-        <Link href={"/"} className="flex items-center">
+        <Link href={"/"} onClick={handleClick} className="flex items-center">
           <h1 className="text-lg font-bold text-zinc-900 dark:text-green-300">
-            ./irvan
+            ./{siteConfig.name}
           </h1>
         </Link>
         <div className="flex gap-4">
           <ul className="flex items-center space-x-4 text-sm text-zinc-500 dark:text-green-300">
-            <Link href={"/"} className="hover:underline">
+            <Link href={"/"} onClick={handleClick} className="hover:underline">
               <p>Home</p>
             </Link>
-            <Link href={"/dashboard"} className="hover:underline">
+            <Link
+              href={"/dashboard"}
+              onClick={handleClick}
+              className="hover:underline"
+            >
               <p>Dashboard</p>
             </Link>
           </ul>
+          <ThemeToggle />
         </div>
       </div>
     </nav>
