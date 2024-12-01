c42205fdd221db4ab9fbf74123055b65e28939c7
0xirvan <darknet1255@gmail.com>
2024-12-01 17:33:09 +0700

    chore: update GitHub link in siteConfig

--- config/siteConfig.ts
+++ config/siteConfig.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
   title: "Irvan Pramana",
   description: "Irvan's personal website",
   links: {
-    github: "https://github.com/0x476569636f",
+    github: "https://github.com/0xirvan",
     facebook: "https://facebook.com/like.a.code",
     linkedin: "https://www.linkedin.com/in/irvan-pramana-putra-3558771b7",
     twitter: "https://x.com/wagbosol",
